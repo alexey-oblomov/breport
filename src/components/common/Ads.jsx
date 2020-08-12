c725ffa4cbb56bc585a3416432f058658d648830
maullerz <maullerz@gmail.com>
2020-08-13 01:45:22 +0300

    fix

--- src/components/common/Ads.jsx
+++ src/components/common/Ads.jsx
@@ -25,9 +25,11 @@ const Ads = ({ type }) => {
     <div className={styles.adsHorizontal}>
       <ins
         className='adsbygoogle'
-        style={{ display: 'inline-block', width: 728, height: 90 }}
+        style={{ display: 'block' }}
         data-ad-client='ca-pub-3299420347078208'
-        data-ad-slot='9657768171'
+        data-ad-slot='1520892318'
+        data-ad-format='auto'
+        data-full-width-responsive='true'
       />
     </div>
   )
